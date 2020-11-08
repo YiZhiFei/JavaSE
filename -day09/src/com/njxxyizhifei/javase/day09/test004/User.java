a4ddbcd6a6275da368a31dc4d1ad480cf117ef91
YiZhiFei <364815799@qq.com>
2020-11-08 18:49:22 +0800

    11.8更新

---
+++ -day09/src/com/njxxyizhifei/javase/day09/test004/User.java
@@ -0,0 +1,24 @@
+package com.njxxyizhifei.javase.day09.test004;
+
+public class User {
+
+	
+	// 无参数构造方法 
+	public User(){ 
+		  System.out.println("User's Default Constructor Invoke!");
+	  }
+	 
+
+	// 有参数的构造方法
+	public User(int i) {
+		System.out.println("带有int类型的构造器");
+	}
+	// 有参数的构造方法
+	public User(String name){
+		System.out.println("带有String类型的构造器");
+	}
+	// 有参数的构造方法
+	public User(int i,String name){
+		System.out.println("带有int,String类型的构造器");		
+	}
+}
